0eb1c2adf08c2d0b89539134ce115139f508a3c4
JT <jturino@gmail.com>
2015-12-17 11:09:45 -0500

    update nb with donation info

--- spec/jobs/nb_create_donation_spec.rb
+++ spec/jobs/nb_create_donation_spec.rb
@@ -1,36 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe NbPersonPushJob, type: :job do
+RSpec.describe NbCreateDonationJob, type: :job do
   describe "#perform" do
-    it "calls NationBuilder with correct args" do
+    it "pushes person to NationBuilder with correct args" do
       args = { attributes: { email: 'user@example.com', phone: '510-555-9999' } }
       stub_nb_method(:create_or_update_person)
 
-      NbPersonPushJob.new.perform(email: 'user@example.com', phone: '510-555-9999')
+      NbCreateDonationJob.new.perform(400, { email: 'user@example.com',
+                                             phone: '510-555-9999' })
 
       expect(Integration::NationBuilder).to have_received(:create_or_update_person).
         with(args)
     end
 
-    it "creates RSVP when event_id is present" do
-      stub_nb_method(:create_or_update_person,
-                     with_args: { attributes: { email: 'user@example.com' } },
-                     returning: { 'id' => 6 })
-      stub_nb_method(:create_rsvp)
-
-      NbPersonPushJob.new.perform(email: 'user@example.com', event_id: 4)
-
-      expect(Integration::NationBuilder).to have_received(:create_rsvp)
-        .with(event_id: 4, person_id: 6)
-    end
-
-    it "creates a donation when donation_amount is present" do
+    it "creates a donation" do
       stub_nb_method(:create_or_update_person,
                      with_args: { attributes: { email: 'user@example.com' } },
                      returning: { 'id' => 6 })
       stub_nb_method(:create_donation)
 
-      NbPersonPushJob.new.perform(email: 'user@example.com', donation_amount: 300 )
+      NbCreateDonationJob.new.perform(300, { email: 'user@example.com' })
 
       expect(Integration::NationBuilder).to have_received(:create_donation).
         with(amount: 300, person_id: 6)
